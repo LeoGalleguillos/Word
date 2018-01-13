2ea374687b531d046b1c416698a918707cd7660e
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-13 08:33:49 -0800

    Create SQL files for thesaurus table

---
+++ sql/leogalle_test/thesaurus/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `thesaurus` (
+      `word_id` int(10) unsigned NOT NULL,
+      `synonym_word_id` int(10) unsigned NOT NULL,
+      PRIMARY KEY (`word_id`, `synonym_word_id`),
+      CONSTRAINT `word_id` FOREIGN KEY (`word_id`) REFERENCES `word` (`word_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+      CONSTRAINT `synonym_word_id` FOREIGN KEY (`synonym_word_id`) REFERENCES `word` (`word_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
