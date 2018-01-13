6d22d2ef9fc0034b7e12199d22606cd82e49615e
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-12 19:38:53 -0800

    Create preliminary thesaurus service class

---
+++ src/Model/Service/Thesaurus.php
@@ -0,0 +1,13 @@
+<?php
+namespace LeoGalleguillos\Word\Model\Service;
+
+use LeoGalleguillos\Word\Model\Entity as WordEntity;
+
+class Thesaurus
+{
+    public function getSynonyms(
+        WordEntity\Word $wordEntity
+    ) {
+
+    }
+}
---
+++ test/Model/Service/ThesaurusTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace LeoGalleguillos\WordTest\Model\Service;
+
+use ArrayObject;
+use LeoGalleguillos\Word\Model\Entity as WordEntity;
+use LeoGalleguillos\Word\Model\Factory as WordFactory;
+use LeoGalleguillos\Word\Model\Service as WordService;
+use LeoGalleguillos\Word\Model\Table as WordTable;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+class ThesaurusTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->thesaurusService = new WordService\Thesaurus(
+            //$this->wordTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            WordService\Thesaurus::class,
+            $this->thesaurusService
+        );
+    }
+}
