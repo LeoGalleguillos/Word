d2b1b23d247223d058c700418912c8dea0574bb8
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-16 08:39:38 -0800

    Start trying to create a service manager for testing

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -3,6 +3,7 @@ namespace LeoGalleguillos\WordTest;
 
 use LeoGalleguillos\Word\Module;
 use PHPUnit\Framework\TestCase;
+use Zend\ServiceManager\ServiceManager;
 
 class ModuleTest extends TestCase
 {
@@ -18,7 +19,13 @@ class ModuleTest extends TestCase
 
     public function testGetServiceConfig()
     {
+        $localConfig   = include(__DIR__ . '/../config/autoload/local.php');
         $serviceConfig = $this->module->getServiceConfig();
+
+        $serviceManager = new ServiceManager();
+        $serviceManager->configure($serviceConfig);
+        //$serviceManager->get(\LeoGalleguillos\Word\Model\Service\Thesaurus::class);
+
         $serviceConfigFactories = $serviceConfig['factories'];
         foreach ($serviceConfigFactories as $className => $value) {
             $this->assertTrue(class_exists($className));
