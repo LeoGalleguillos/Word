cbef696dfab5bc1f7dbd6aff102fa96e31f224a0
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-16 11:58:53 -0800

    Use memcached to store xml strings

--- src/Model/Service/Api/DictionaryApiCom/Thesaurus.php
+++ src/Model/Service/Api/DictionaryApiCom/Thesaurus.php
@@ -1,21 +1,39 @@
 <?php
 namespace LeoGalleguillos\Word\Model\Service\Api\DictionaryApiCom;
 
+use LeoGalleguillos\Memcached\Model\Service as MemcachedService;
 use SimpleXMLElement;
 
 class Thesaurus
 {
     public function __construct(
+        MemcachedService\Memcached $memcached,
         string $apiKey
     ) {
-        $this->apiKey = $apiKey;
+        $this->memcached = $memcached;
+        $this->apiKey    = $apiKey;
     }
 
-    public function getXml(string $word)
+    protected function getXml(string $word)
     {
-        $url = 'https://www.dictionaryapi.com'
-             . '/api/v1/references/thesaurus/xml/test?key='
-             . $this->apiKey;
-        return simplexml_load_file($url);
+        return new SimpleXmlElement(
+            $this->getXmlString($word)
+        );
+    }
+
+    protected function getXmlString(string $word)
+    {
+        $cacheKey = md5(__METHOD__ . $word);
+        if (null !== ($xmlString = $this->memcached->get($cacheKey))) {
+            return $xmlString;
+        }
+
+        $url       = 'https://www.dictionaryapi.com'
+                   . '/api/v1/references/thesaurus/xml/test?key='
+                   . $this->apiKey;
+        $xmlString = file_get_contents($url);
+
+        $this->memcached->setForMinutes($cacheKey, $xmlString, 3);
+        return $xmlString;
     }
 }
--- test/Model/Service/Api/DictionaryApiCom/ThesaurusTest.php
+++ test/Model/Service/Api/DictionaryApiCom/ThesaurusTest.php
@@ -2,20 +2,24 @@
 namespace LeoGalleguillos\WordTest\Model\Service\Api\DictionaryApiCom;
 
 use ArrayObject;
+use LeoGalleguillos\Memcached\Model\Service as MemcachedService;
 use LeoGalleguillos\Word\Model\Entity as WordEntity;
 use LeoGalleguillos\Word\Model\Factory as WordFactory;
 use LeoGalleguillos\Word\Model\Service as WordService;
 use LeoGalleguillos\Word\Model\Table as WordTable;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 use SimpleXMLElement;
 
 class ThesaurusTest extends TestCase
 {
     protected function setUp()
     {
+        $this->memcached = new MemcachedService\Memcached();
         $configArray = require(__DIR__ . '/../../../../../config/autoload/local.php');
         $apiKey      = $configArray['dictionaryapicom']['api_key'];
         $this->thesaurusApi = new WordService\Api\DictionaryApiCom\Thesaurus(
+            $this->memcached,
             $apiKey
         );
     }
@@ -30,9 +34,16 @@ class ThesaurusTest extends TestCase
 
     public function testGetXml()
     {
+        $reflectionClass = new ReflectionClass($this->thesaurusApi);
+        $reflectionMethod = $reflectionClass->getMethod('getXml');
+        $reflectionMethod->setAccessible(true);
+        $simpleXmlElement = $reflectionMethod->invokeArgs(
+            $this->thesaurusApi,
+            ['test']
+        );
         $this->assertInstanceOf(
             SimpleXMLElement::class,
-            $this->thesaurusApi->getXml('test')
+            $simpleXmlElement
         );
     }
 }
