fc6ff5b816305198179f82959a1aba6d1f60d2c0
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-17 07:03:38 -0800

    Write and test method to get entity from string

--- src/Model/Service/Word.php
+++ src/Model/Service/Word.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\Word\Model\Service;
 
+use LeoGalleguillos\Word\Model\Factory as WordFactory;
 use LeoGalleguillos\Word\Model\Table as WordTable;
 
 class Word
@@ -9,9 +10,21 @@ class Word
      * Construct.
      */
     public function __construct(
+        WordFactory\Word $wordFactory,
         WordTable\Word $wordTable
     ) {
-        $this->wordTable      = $wordTable;
+        $this->wordFactory = $wordFactory;
+        $this->wordTable   = $wordTable;
+    }
+
+    public function getEntityFromString(string $word)
+    {
+        if ($this->isWordInTable($word)) {
+            return $this->wordFactory->buildFromWord($word);
+        }
+
+        $this->wordTable->insertIgnore($word);
+        return $this->wordFactory->buildFromWord($word);
     }
 
     /**
--- test/Model/Service/WordTest.php
+++ test/Model/Service/WordTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace LeoGalleguillos\WordTest\Model\Service;
 
+use LeoGalleguillos\Word\Model\Entity as WordEntity;
+use LeoGalleguillos\Word\Model\Factory as WordFactory;
 use LeoGalleguillos\Word\Model\Service as WordService;
 use LeoGalleguillos\Word\Model\Table as WordTable;
 use PHPUnit\Framework\TestCase;
@@ -9,10 +11,14 @@ class WordTest extends TestCase
 {
     protected function setUp()
     {
+        $this->wordFactoryMock = $this->createMock(
+            WordFactory\Word::class
+        );
         $this->wordTableMock = $this->createMock(
             WordTable\Word::class
         );
         $this->wordService = new WordService\Word(
+            $this->wordFactoryMock,
             $this->wordTableMock
         );
     }
@@ -22,6 +28,46 @@ class WordTest extends TestCase
         $this->assertInstanceOf(
             WordService\Word::class,
             $this->wordService
+        );
+    }
+
+    public function testGetEntityFromString()
+    {
+        $wordEntity1         = new WordEntity\Word();
+        $wordEntity1->wordId = 1;
+        $wordEntity1->word   = 'test';
+
+        $wordEntity2         = new WordEntity\Word();
+        $wordEntity2->wordId = 2;
+        $wordEntity2->word   = 'essay';
+
+        $wordEntity3         = new WordEntity\Word();
+        $wordEntity3->wordId = 3;
+        $wordEntity3->word   = 'trial';
+
+        $this->wordTableMock->method('selectCountWhereWord')->will(
+            $this->onConsecutiveCalls(1, 0, 1)
+        );
+
+        $this->wordFactoryMock->method('buildFromWord')->will(
+            $this->onConsecutiveCalls($wordEntity1, $wordEntity2, $wordEntity3)
+        );
+
+        $this->wordTableMock->expects($this->once())->method('insertIgnore');
+
+        $this->assertEquals(
+            $wordEntity1,
+            $this->wordService->getEntityFromString($wordEntity1->word)
+        );
+
+        $this->assertEquals(
+            $wordEntity2,
+            $this->wordService->getEntityFromString($wordEntity2->word)
+        );
+
+        $this->assertEquals(
+            $wordEntity3,
+            $this->wordService->getEntityFromString($wordEntity3->word)
         );
     }
 
